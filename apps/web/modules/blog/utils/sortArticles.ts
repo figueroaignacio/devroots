9b3647705cf15c04a3a006de68521adfb9c93766
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-12-27 19:12:33 -0300

    chore: remove utils folder

--- apps/web/modules/blog/utils/sortArticles.ts
+++
@@ -1,9 +0,0 @@
-import { Blog } from "@content";
-
-export function sortArticles(articles: Blog[]) {
-  return articles.sort((a, b) => {
-    if (a.date > b.date) return -1;
-    if (a.date > b.date) return 1;
-    return 0;
-  });
-}
