1bce984a69b337e07bc31d9128642e7282a2a62f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-12-28 17:38:41 -0300

    feat: add images to spotlights card

--- apps/web/modules/landing/sections/spotlights.tsx
+++ apps/web/modules/landing/sections/spotlights.tsx
@@ -13,6 +13,12 @@ export const Spotlights = () => {
   const t = useTranslations();
   const spotlights = t.raw("spotlights");
 
+  const images = [
+    "/magicpattern-grid-pattern-1735417074146.png",
+    "/magicpattern-grid-pattern-1735417406009.png",
+    "/magicpattern-grid-pattern-1735417346535.png",
+  ];
+
   return (
     <div>
       <ul className="grid grid-cols-2 lg:grid-cols-12 gap-3">
@@ -28,6 +34,7 @@ export const Spotlights = () => {
             <SpotlightCard
               title={spotlight.title}
               description={spotlight.description}
+              img={images[Math.floor(Math.random() * images.length)]}
             />
           </li>
         ))}
--- apps/web/modules/landing/ui/spotlight-card.tsx
+++ apps/web/modules/landing/ui/spotlight-card.tsx
@@ -1,5 +1,4 @@
 // Components
-import { Waves } from "@/components/shared/waves";
 import {
   Card,
   CardContent,
@@ -7,22 +6,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 interface SpotlightCardProps {
   title: string;
   description: string;
+  img: string;
 }
 
-export function SpotlightCard({ description, title }: SpotlightCardProps) {
+export function SpotlightCard({ description, title, img }: SpotlightCardProps) {
   return (
-    <Card className="w-full h-full overflow-hidden">
-      <CardHeader className="">
+    <Card className="w-full h-full overflow-hidden relative z-50">
+      <Image
+        width={400}
+        height={400}
+        src={img}
+        alt={title}
+        className="w-full h-full absolute inset-0 object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 opacity-30"
+      />
+      <CardHeader>
         <CardTitle className="text-3xl w-72 font-bold">{title}</CardTitle>
       </CardHeader>
       <CardContent>
         <CardDescription>{description}</CardDescription>
       </CardContent>
-      <Waves />
     </Card>
   );
 }
