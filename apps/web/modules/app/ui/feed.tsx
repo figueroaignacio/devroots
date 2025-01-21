d41bd774708c1dc884eb87e11866828eadda08e1
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-21 02:05:10 -0300

    chore: page test

--- apps/web/modules/app/ui/feed.tsx
+++ apps/web/modules/app/ui/feed.tsx
@@ -1,12 +1,13 @@
-/* eslint-disable @next/next/no-img-element */
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { API_URL } from "@/lib/constants";
 import { auth } from "@/modules/auth/lib/auth";
 import { format } from "date-fns";
+import { getInitials } from "../lib/utils";
 
 interface User {
   id: string;
   name: string;
-  email: string;
+  email: string | null;
   password: string | null;
   emailVerified: string;
   image: string | null;
@@ -25,37 +26,34 @@ export async function Feed() {
     <div className="flex flex-col gap-y-4">
       <div>
         <h2>Account</h2>
-        <pre className="space-y-6">{JSON.stringify(session, null, 2)}</pre>
+        <pre>
+          <code className="p-2">{JSON.stringify(session, null, 2)}</code>
+        </pre>
       </div>
       <div>
         <h2 className="mb-5">Users registered</h2>
         <ul className="space-y-5">
           {users.map((user) => (
             <li key={user.id} className="bg-card p-4 rounded-md">
-              {user.image ? (
-                <img
-                  src={user.image || "https://via.placeholder.com/56"}
-                  alt={`${user.name}'s avatar`}
-                  className="size-7 rounded-full"
-                />
-              ) : (
-                <p>No image provided</p>
-              )}
+              <Avatar>
+                <AvatarImage src={user.image || undefined} alt={user.name} />
+                <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+              </Avatar>
               <h3 className="font-semibold">{user.name}</h3>
               <p className="text-gray-600">{user.email}</p>
               <p>
                 <span className="font-medium">Verified:</span>{" "}
                 {user.emailVerified
-                  ? format(new Date(user.emailVerified), "PPPpp")
+                  ? format(new Date(user.emailVerified), "PPP p")
                   : "Not verified"}
               </p>
               <p>
                 <span className="font-medium">Created At:</span>{" "}
-                {format(new Date(user.createdAt), "PPPpp")}
+                {format(new Date(user.createdAt), "PPP p")}
               </p>
               <p>
                 <span className="font-medium">Updated At:</span>{" "}
-                {format(new Date(user.updatedAt), "PPPpp")}
+                {format(new Date(user.updatedAt), "PPP p")}
               </p>
             </li>
           ))}
