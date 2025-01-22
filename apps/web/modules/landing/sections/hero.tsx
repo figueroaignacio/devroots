b1f9181ddd6b7420a6c0b4bd3935e3ee128f0a10
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-22 15:13:16 -0300

    style: hero improvements

--- apps/web/modules/landing/sections/hero.tsx
+++ apps/web/modules/landing/sections/hero.tsx
@@ -9,7 +9,7 @@ export function Hero() {
   const t = useTranslations();
 
   return (
-    <div className="overflow-x-hidden lg:min-h-[75dvh] flex justify-center items-center relative">
+    <div className="overflow-x-hidden lg:min-h-[70dvh] flex justify-center items-center relative">
       <div className="absolute inset-0 -z-50">
         <img
           className="h-full w-full object-cover opacity-10"
@@ -18,15 +18,15 @@ export function Hero() {
         />
       </div>
 
-      <section className="relative">
+      <section className="relative px-8">
         <div className="mx-auto max-w-7xl">
           <div className="mx-auto max-w-2xl text-center">
-            <h1 className="font-inter  text-lg">
+            <h1 className="text-lg font-semibold">
               {t.rich("hero.title", {
                 span: (chunks) => <span>{chunks}</span>,
               })}
             </h1>
-            <p className="mt-5 text-4xl font-bold leading-tight sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight relative">
+            <p className="mt-5 text-4xl font-extrabold leading-tight sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight relative">
               <span>{t("hero.subtitle")}</span>
               <span className="relative">
                 <span className="absolute inset-0 h-full w-full scale-90 bg-gradient-to-r from-violet-400 via-pink-400 to-violet-300 opacity-30 blur-xl filter -z-40"></span>
