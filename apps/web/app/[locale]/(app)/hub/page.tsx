ac079a55a6c11c999b0d80eec261670bc6a4886a
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-23 15:02:34 -0300

    style: add border

--- apps/web/app/[locale]/(app)/hub/page.tsx
+++ apps/web/app/[locale]/(app)/hub/page.tsx
@@ -11,13 +11,13 @@ export default async function HubPage() {
   }
 
   return (
-    <div className="container mx-auto">
+    <div className="px-3 mx-auto min-h-screen overflow-visible">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
         <main className="lg:col-span-7 space-y-6">
           <h1 className="text-3xl font-bold mb-6">Feed</h1>
           <Feed />
         </main>
-        <aside className="lg:col-span-5 space-y-6 mt-14">
+        <aside className="lg:col-span-5 lg:sticky lg:top-16 lg:h-full space-y-6 lg:border-l lg:pl-8">
           <UsersList />
         </aside>
       </div>
