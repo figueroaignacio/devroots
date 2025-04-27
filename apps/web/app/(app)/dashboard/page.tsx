02fb2197786a221e22ce17ad6da5682f5f34b2f9
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-04-26 22:44:39 -0300

    feat: add dashboard

--- apps/web/app/(app)/dashboard/page.tsx
+++ apps/web/app/(app)/dashboard/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function DashboardPage() {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/api/auth/me", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <main>
-      <h1>Dashboard page</h1>
+    <main className="flex flex-col items-center justify-between p-24 min-h-full">
+      <h1>Hello {user.firstName}</h1>
+      {user ? (
+        <pre className="border max-w-3xl p-4 overflow-x-auto rounded-md">
+          <code className="">{JSON.stringify(user, null, 2)}</code>
+        </pre>
+      ) : (
+        <p>No user data available</p>
+      )}
     </main>
   );
 }
