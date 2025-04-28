8f15814b0b87592b6566b14bfce5f856808553fe
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-04-28 17:39:47 -0300

    style: add better auth layout

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -8,7 +8,9 @@ export default function AuthLayout({
   return (
     <>
       <BackButton />
-      {children}
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">{children}</div>
+      </div>
     </>
   );
 }
