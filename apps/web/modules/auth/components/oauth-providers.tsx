afbde01eeaffb9be431aaccf2018222d86e024b0
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-04-28 17:37:41 -0300

    style: change oauth providers button styles

--- apps/web/modules/auth/components/oauth-providers.tsx
+++ apps/web/modules/auth/components/oauth-providers.tsx
@@ -1,14 +1,16 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Button } from "@workspace/ui/components/button";
+import { GoogleLogoIcon } from "./icons";
 
 export function OAuthProviders() {
   return (
-    <>
-      <Button variant="outline" className="w-full" type="button">
-        Register with Google
+    <div className="grid grid-cols-2">
+      <Button variant="outline" type="button" className="w-full rounded-none">
+        <GitHubLogoIcon />
       </Button>
-      <Button variant="outline" className="w-full" type="button">
-        Register with GitHub
+      <Button variant="outline" type="button" className="w-full rounded-none">
+        <GoogleLogoIcon />
       </Button>
-    </>
+    </div>
   );
 }
