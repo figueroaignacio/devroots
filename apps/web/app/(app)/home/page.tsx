8f9019377b9042278b6d2265cbb7faebc47262c8
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-05 21:34:19 -0300

    chore: remove

--- apps/web/app/(app)/home/page.tsx
+++ apps/web/app/(app)/home/page.tsx
@@ -10,12 +10,7 @@ export default function DashboardPage() {
   const { user } = useAuthStore();
 
   return (
-    <section className="space-y-12 mt-24">
-      <div className="bg-slate-900 rounded-lg p-4 text-sm font-mono text-slate-300 overflow-x-auto">
-        <pre>
-          <code>{JSON.stringify(user, null, 2)}</code>
-        </pre>
-      </div>
+    <section>
       <LogoutButton />
     </section>
   );
