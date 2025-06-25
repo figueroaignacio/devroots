adbd7a72a7031ae8f5f56ffd394bbd1d54dfd185
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-06-25 16:32:41 -0300

    feat: finish users module

--- apps/api/src/modules/users/dto/create-user.dto.ts
+++ apps/api/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  avatar_url?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+}
