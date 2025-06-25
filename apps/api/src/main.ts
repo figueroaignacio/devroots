dfcee234994b14dfff8f91e3a2425f151b2177a4
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-06-25 15:16:30 -0300

    feat: add api security and docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -6,6 +8,22 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3001);
 
   app.setGlobalPrefix('/api');
+  app.use(helmet());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 }
 
 bootstrap();
