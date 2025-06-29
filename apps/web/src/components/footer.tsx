9baad17da4764c8e60dfa1678134b3851fe8aa58
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-06-29 18:27:28 -0300

    feat: add toggle theme component

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,10 +1,13 @@
+import { ToggleTheme } from './toggle-theme';
+
 export function Footer() {
   return (
     <footer className="border-border border-t">
-      <div className="container mx-auto px-4 py-8 text-center">
+      <div className="container mx-auto space-y-12 px-4 py-8 text-center">
         <p className="text-muted-foreground text-sm">
           &copy; {new Date().getFullYear()} Devroots. All rights reserved.
         </p>
+        <ToggleTheme />
       </div>
     </footer>
   );
---
+++ apps/web/src/components/toggle-theme.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+// Hooks
+import { useEffect, useState } from 'react';
+
+// Components
+import { Button } from '@/components/ui/button';
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+export function ToggleTheme() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      <SunIcon className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <MoonIcon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
