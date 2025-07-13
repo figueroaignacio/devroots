657129f7b969c82d65c563494c550c2bbe77e89f
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-07-12 22:52:20 -0300

    chore: archive project :c

--- apps/web/src/app/(auth)/auth/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { OAuthLoginForm } from '@/modules/auth/components/oauth-login-form';
-
-export default function LoginPage() {
-  return <OAuthLoginForm />;
-}
--- apps/web/src/modules/lobby/components/background-decoration.tsx
+++
@@ -1,7 +0,0 @@
-export function BackgroundDecoration() {
-  return (
-    <>
-      <div className="absolute top-0 right-0 left-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-blue-400 opacity-20 blur-[100px]"></div>
-    </>
-  );
-}
